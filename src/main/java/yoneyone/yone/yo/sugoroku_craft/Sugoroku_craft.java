84cb1e8638958586ad33b844d2ba9ec3f500b464
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 16:12:54 +0900

    switch文に変更、その他微調整 サイコロを振る部分の一部作成

--- src/main/java/yoneyone/yone/yo/sugoroku_craft/Sugoroku_craft.java
+++ src/main/java/yoneyone/yone/yo/sugoroku_craft/Sugoroku_craft.java
@@ -1,5 +1,6 @@
 package yoneyone.yone.yo.sugoroku_craft;
 
+import com.google.gson.internal.$Gson$Preconditions;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.CommandBlock;
 import org.bukkit.command.BlockCommandSender;
@@ -24,59 +25,75 @@ public final class Sugoroku_craft extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equals("test")) {
-            if (sender instanceof BlockCommandSender) {
-                BlockCommandSender blockCommandSender = (BlockCommandSender) sender;
-                BlockState blockState = blockCommandSender.getBlock().getState();
-                CommandBlock commandBlock = (CommandBlock) blockState;
-                commandBlock.setCommand("テストは成功しました");
-                commandBlock.update();
-            }else {
-                sender.sendMessage("このコマンドはコマンドブロックから実行してください");
-            }
-        }else if (label.equals("masu")){
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                try {
-                    String d_path = "すごろくデータ\\"+ args[0];
-                    if ((new File("すごろくデータ")).mkdir()){
-                        System.out.println("すごろくデータディレクトリを作成しました");
-                    }
-                    if (new File(d_path).mkdir()){
-                        player.sendMessage("新しいすごろく場を生成しました");
-                    }
-                    String[] file_list = new File(d_path).list();
-                    int i = -1;
-                    if (file_list != null) {
-                        for (String str : file_list) {
-                            int l = Integer.parseInt(str);
-                            if (l > i) {
-                                i = l;
+        switch (label) {
+            case "test":
+                if (sender instanceof BlockCommandSender) {
+                    BlockCommandSender blockCommandSender = (BlockCommandSender) sender;
+                    BlockState blockState = blockCommandSender.getBlock().getState();
+                    CommandBlock commandBlock = (CommandBlock) blockState;
+                    commandBlock.setCommand("テストは成功しました");
+                    commandBlock.update();
+                } else {
+                    sender.sendMessage("このコマンドはコマンドブロックから実行してください");
+                }
+                break;
+            case "masu":
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    try {
+                        String d_path = "すごろくデータ\\" + args[0];
+                        if ((new File("すごろくデータ")).mkdir()) {
+                            System.out.println("すごろくデータディレクトリを作成しました");
+                        }
+                        if (new File(d_path).mkdir()) {
+                            player.sendMessage("新しいすごろく場を生成しました");
+                        }
+                        String[] file_list = new File(d_path).list();
+                        int i = -1;
+                        if (file_list != null) {
+                            for (String str : file_list) {
+                                int l = Integer.parseInt(str);
+                                if (l > i) {
+                                    i = l;
+                                }
                             }
                         }
-                    }
-                    i += 1;
-                    String file_path = "すごろくデータ\\"+ args[0] +"\\"+ i;
-                    try (FileWriter fw = new FileWriter(file_path)){
-                        String x = String.valueOf(player.getLocation().getX());
-                        String y = String.valueOf(player.getLocation().getY());
-                        String z = String.valueOf(player.getLocation().getZ());
-                        fw.write(x);
-                        fw.write(ln);
-                        fw.write(y);
-                        fw.write(ln);
-                        fw.write(z);
-                        player.sendMessage("マス「"+ i +"」を保存しました");
-                    }catch (IOException e){
-                        player.sendMessage("ファイル書きこみエラーが発生しました");
+                        i += 1;
+                        String file_path = "すごろくデータ\\" + args[0] + "\\" + i;
+                        try (FileWriter fw = new FileWriter(file_path)) {
+                            String x = String.valueOf(player.getLocation().getX());
+                            String y = String.valueOf(player.getLocation().getY());
+                            String z = String.valueOf(player.getLocation().getZ());
+                            fw.write(x);
+                            fw.write(ln);
+                            fw.write(y);
+                            fw.write(ln);
+                            fw.write(z);
+                            player.sendMessage("マス「" + i + "」を保存しました");
+                        } catch (IOException e) {
+                            player.sendMessage("ファイル書きこみエラーが発生しました");
+                            return true;
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        player.sendMessage("コマンドの使い方を間違えています。" + ln + "正しい使い方/masu [すごろくの名前]");
                         return true;
                     }
-                }catch (ArrayIndexOutOfBoundsException e){
-                    player.sendMessage("コマンドの使い方を間違えています。\n正しい使い方/masu [すごろくの名前]");
-                    return true;
+                } else {
+                    sender.sendMessage("このコマンドはプレイヤーから実行してください");
                 }
-            }
+                break;
+            case "sgo":
+                if (sender instanceof Player){
+                    Player player = (Player) sender;
+                    dice_GUI(player);
+                } else {
+                    sender.sendMessage("このコマンドはプレイヤーから実行してください");
+                }
+                break;
         }
         return true;
+    }
+    private void dice_GUI(Player player){
+        //ここにサイコロを実装
     }
 }
